5ee9aa00bce2e13ac2c161b899a50789d9addee0
Rabbit <65748233+unlinedrabbit82@users.noreply.github.com>
2025-12-03 20:31:58 +0000

    js

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: false,
+      speed: 1,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 100
+    });
+
+
+});
